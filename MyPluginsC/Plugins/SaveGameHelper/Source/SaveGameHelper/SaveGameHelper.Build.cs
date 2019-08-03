a8dc32e04b5fbe9b96d899e0ab526c5298ca9dde
Hybrid The Akuma <tkwat11@gmail.com>
2019-08-02 20:48:12 -0600

    Fixed Save Game Plugin
    P.S. C++ in UE4 sucks. BP's are much better, and C# is the best ever.
    -end rant

--- MyPluginsC/Plugins/SaveGameHelper/Source/SaveGameHelper/SaveGameHelper.Build.cs
+++ MyPluginsC/Plugins/SaveGameHelper/Source/SaveGameHelper/SaveGameHelper.Build.cs
@@ -4,58 +4,62 @@ using UnrealBuildTool;
 
 public class SaveGameHelper : ModuleRules
 {
-	public SaveGameHelper(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				"SaveGameHelper/Public",
-				//"Engine/Source/Runtime/Core/Public/Misc/Paths.h",
-				
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				"SaveGameHelper/Private",
-				//"Engine/Source/Runtime/Core/Private/Misc/Paths.cpp",
-				
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",// "Engine"
-				
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
+   public SaveGameHelper(ReadOnlyTargetRules Target) : base(Target)
+   {
+      PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+      PublicIncludePaths.AddRange(
+         new string[] {
+            "SaveGameHelper/Public",
+            "Runtime/Core/Public/Misc",
+            "Runtime/Core/Public/HAL"
+            //"Engine/Source/Runtime/Core/Public/Misc/Paths.h",
+            
+            // ... add public include paths required here ...
+         }
+         );
+
+
+      PrivateIncludePaths.AddRange(
+         new string[] {
+            "SaveGameHelper/Private"
+            //"Runtime/Core/Private/Misc/Paths.cpp"
+            //"Engine/Source/Runtime/Core/Private/Misc/Paths.cpp",
+            
+            // ... add other private include paths required here ...
+         }
+         );
+
+
+      PublicDependencyModuleNames.AddRange(
+         new string[]
+         {
+            "Core",
+            "SaveGameHelper"
+            
+            // ... add other public dependencies that you statically link with here ...
+         }
+         );
+
+
+      PrivateDependencyModuleNames.AddRange(
+         new string[]
+         {
+            "CoreUObject",
+            "Engine",
+            "Slate",
+            "SlateCore",
+
+            // ... add private dependencies that you statically link with here ...	
+         }
+         );
+
+
+      DynamicallyLoadedModuleNames.AddRange(
+         new string[]
+         {
+            // ... add any modules that your module loads dynamically here ...
+         }
+         );
+   }
 }
---
+++ PackagedPlugins/SGH/v4-22/SaveGameHelper/Intermediate/Build/Win64/UE4/Inc/SaveGameHelper/SaveGameHelper.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSaveGameHelper_init() {}
+	UPackage* Z_Construct_UPackage__Script_SaveGameHelper()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/SaveGameHelper",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x7A99F39C,
+				0x6809A123,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
